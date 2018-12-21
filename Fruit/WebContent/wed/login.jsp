f754a12604d101916d49e753e95727fe2cc87fc2
zgy <2250034912@qq.com>
2018-12-21 19:10:12 +0800

    7

--- Fruit/WebContent/wed/login.jsp
+++ Fruit/WebContent/wed/login.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="zh">
 <head>
@@ -26,7 +27,8 @@
 				<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">登录</label>
 				<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">注册</label>
 				<div class="login-form">
-					<form>
+					<form action="<%=request.getContextPath() %>/userLogin.s" method="post">
+					<input name="op" type="hidden" value="login"/>
 					<div class="sign-in-htm">
 						<div class="group">
 							<label for="user" class="label">用户名</label>
@@ -34,20 +36,14 @@
 						</div>
 						<div class="group">
 							<label for="pass" class="label">密码</label>
-							<input id="password" type="password" class="input" data-type="password">
+							<input id="password" name="password" type="password" class="input" data-type="password">
 						</div>
 						<div class="group">
-							<div class="container">
-							    <div id="captcha" style="position: relative" data-type="password"></div>
-								<div id="msg"></div>
-							</div>
-						</div>
-						<div class="group">
-							<input id="check" type="checkbox" class="check" checked>
+							<input id="check" type="checkbox" class="check" name="checkbox" checked>
 							<label for="check"><span class="icon"></span> 保持登录</label>
 						</div>						
 						<div class="group">
-							<input type="button" class="button" value="登录" onClick="sub()">
+							<input type="submit" class="button" value="登录" >
 						</div>
 						<div class="hr"></div>
 						<div class="foot-lnk">
@@ -112,3 +108,8 @@
 </script>
 </body>
 </html>
+	<c:if test="${! empty msg }">
+		<script type="text/javascript">
+			alert('${msg}');
+		</script>
+	</c:if>
---
+++ Fruit/src/servlet/userLogin.java
@@ -0,0 +1,76 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Bean.user;
+import Bean.worker;
+import Bize.BizeLogin;
+import Bize.BizeMethod;
+import Expection.LoginException;
+
+
+@WebServlet("/userLogin.s")
+public class userLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		String op = request.getParameter("op");
+		if("login".equals(op)) {
+			login(request,response);
+		}
+	}
+
+	
+	private void login(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String name = request.getParameter("username"); 
+		String pwd = request.getParameter("password");
+		
+		
+		//String s = (String) request.getSession().getAttribute("piccode");
+		String[] arr = request.getParameterValues("checkbox");
+		if (arr != null) {
+			Cookie cookie = new Cookie("username", name);
+			cookie.setMaxAge(7 * 3600 * 24);
+			response.addCookie(cookie);
+		}
+
+		Cookie[] myCookie = request.getCookies();
+		if (myCookie != null) {
+			for (int i = 0; i < myCookie.length; i++) {
+				if (myCookie[i].getValue().equals(name)) {
+					request.getSession().setAttribute("longinUser", name);
+					System.out.println(request.getSession().getAttribute("longinUser"));
+					request.getRequestDispatcher("wed/index.jsp").forward(request, response);
+				}
+			}
+		}
+		 user users = BizeLogin.login(name, pwd);
+
+				if (users != null) {
+					request.getSession().setAttribute("longinUser", name);
+					request.getRequestDispatcher("wed/index.jsp").forward(request, response);
+				} else {
+					request.setAttribute("msg", "账号或密码错误！");
+					request.getRequestDispatcher("wed/login.jsp").forward(request, response);
+				}
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
